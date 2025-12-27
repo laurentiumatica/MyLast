7a7ac5507a2a0c8f54d005b1a2cbbd256298acd0
alexandra <alexandramariab99@gmail.com>
2025-12-27 11:11:19 +0000

    am adaugat extragerea userilor si actiunea de login, respectiv logout

--- mylast.sh
+++ mylast.sh
@@ -7,7 +7,32 @@ echo "MyLast started!"
 parse() {
     while read -r linie; do
         timestamp=$(echo "$linie" | awk '{print $1}')
-        echo "$timestamp"
+#extragem si userul, adresam problema pe mai multe cazuri
+     user=""
+     action=""
+case "$linie" in
+*"session opened"*)
+            user=$(awk '{for(i=1;i<=NF;i++) if($i=="user") print $(i+1)}' <<< "$linie")
+            action="LOGIN"
+	    ;;
+        *"session closed"*)
+            user=$(awk '{for(i=1;i<=NF;i++) if($i=="user") print $(i+1)}' <<< "$linie")
+            action="LOGOUT"
+            ;;
+        *"Accepted"*)
+            user=$(awk '{for(i=1;i<=NF;i++) if($i=="for") print $(i+1)}' <<< "$linie")
+            action="LOGIN"
+            ;;
+        *"Failed"*)
+            user=$(awk '{for(i=1;i<=NF;i++) if($i=="for") print $(i+1)}' <<< "$linie")
+            ;;
+        *"Disconnected from"*)
+            user=$(awk '{for(i=1;i<=NF;i++) if($i=="user") print $(i+1)}' <<< "$linie")
+            action="LOGOUT"
+            ;;
+    esac
+
+   echo "timestamp=$timestamp user=$user action=$action"
     done < "$1"
 }
-parse /var/log/auth.log
+parse tests/auth.log.sample
