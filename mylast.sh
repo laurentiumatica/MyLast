47718c5e4107bba12000d5ef35125c3364d6fa44
alexandra <alexandramariab99@gmail.com>
2025-12-29 12:51:37 +0000

    am facut asocierea Login-Logout pentru fiecare user

--- mylast.sh
+++ mylast.sh
@@ -5,34 +5,59 @@
 echo "MyLast started!"
 #extragem momentan doar timestamp-ul din fisier
 parse() {
-    while read -r linie; do
-        timestamp=$(echo "$linie" | awk '{print $1}')
-#extragem si userul, adresam problema pe mai multe cazuri
-     user=""
-     action=""
-case "$linie" in
-*"session opened"*)
-            user=$(awk '{for(i=1;i<=NF;i++) if($i=="user") print $(i+1)}' <<< "$linie")
-            action="LOGIN"
-	    ;;
-        *"session closed"*)
-            user=$(awk '{for(i=1;i<=NF;i++) if($i=="user") print $(i+1)}' <<< "$linie")
-            action="LOGOUT"
-            ;;
-        *"Accepted"*)
-            user=$(awk '{for(i=1;i<=NF;i++) if($i=="for") print $(i+1)}' <<< "$linie")
-            action="LOGIN"
-            ;;
-        *"Failed"*)
-            user=$(awk '{for(i=1;i<=NF;i++) if($i=="for") print $(i+1)}' <<< "$linie")
-            ;;
-        *"Disconnected from"*)
-            user=$(awk '{for(i=1;i<=NF;i++) if($i=="user") print $(i+1)}' <<< "$linie")
-            action="LOGOUT"
-            ;;
-    esac
+login_user=""
+login_time=""
+logout_time=""
 
-   echo "timestamp=$timestamp user=$user action=$action"
-    done < "$1"
+    while read -r linie; do
+        timestamp=$(awk '{print $1}' <<< "$linie")
+#extragem si userul, adresam problema pe mai multe cazuri
+        user=""
+        action=""
+        case "$linie" in
+            *"session opened for user "*)
+                user=$(awk '{for(i=1;i<=NF;i++) if($i=="user") {print $(i+1); break}}' <<< "$linie")
+                action="LOGIN"
+                ;;
+            *"session closed for user "*|*"Disconnected from user "*)
+                user=$(awk '{for(i=1;i<=NF;i++) if($i=="user") {print $(i+1); break}}' <<< "$linie")
+                action="LOGOUT"
+                ;;
+            *"Accepted password for "*|*"Accepted publickey for "*)
+                user=$(awk '{for(i=1;i<=NF;i++) if($i=="for") {print $(i+1); break}}' <<< "$linie")
+                action="LOGIN"
+                ;;
+            *)
+                continue
+                ;;
+        esac
+        
+       if [[ "$user" == *root* || "$user" == *gdm* ]]; then
+          continue
+       fi
+
+       echo "timestamp=$timestamp user=$user action=$action"
+        if [[ "$action" == "LOGIN" ]]; then
+        login_user="$user"
+        login_time="$timestamp"
+        logout_time="still running"
+    elif [[ "$action" == "LOGOUT" ]]; then
+        if [[ "$login_user" == "$user" ]]; then
+            logout_time="$timestamp"
+            echo "User: $login_user, Login: $login_time, Logout: $logout_time"
+      
+            login_user=""
+            login_time=""
+            logout_time=""
+        fi
+    fi
+ done < "$1"
+
+  if [[ -n "$login_user" ]]; then
+        echo "User: $login_user, Login: $login_time, Logout: $logout_time"
+    fi
 }
-parse tests/auth.log.sample
+
+
+parse "$1"
+
